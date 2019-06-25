e143515da1c4b898fa3e168c80357f9b1f274019
Grishma Kothari <Grishma@Sharmilkumars-MacBook-Pro.local>
2019-06-25 19:14:29 -0400

    Header Component Visual

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipesComponent,
+    ShoppingListComponent
   ],
   imports: [
     BrowserModule,
